757cb7693b9005e59725fc230582e968f0225dd0
wenjebs <wenjieb2@gmail.com>
2024-05-20 20:13:21 +0800

    Intialise marker to current position on run page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
-import 'package:runningapp/pages/auth_page.dart';
+import 'package:runningapp/pages/login_and_registration/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
