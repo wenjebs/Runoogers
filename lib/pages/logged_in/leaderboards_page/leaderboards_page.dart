a076757d65b86da7b50984b513dc6890a86b9a78
wenjebs <wenjieb2@gmail.com>
2024-06-27 21:44:13 +0800

    skeleton for leaderboard

--- lib/pages/logged_in/leaderboards_page/leaderboards_page.dart
+++ lib/pages/logged_in/leaderboards_page/leaderboards_page.dart
@@ -5,12 +5,146 @@ class LeaderboardsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-          child: Text(
-        "LEADERBOARDS HERE",
-        style: TextStyle(fontSize: 20),
-      )),
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: SearchBar(
+              leading: Icon(Icons.search),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsetsDirectional.all(30),
+                    child: ListView.builder(
+                      padding: EdgeInsets.zero,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      itemBuilder: (context, index) {
+                        return child(
+                            index: index +
+                                1); // Pass the index to the child widget
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class child extends StatelessWidget {
+  final int index;
+  const child({
+    super.key,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 1),
+      child: Container(
+        width: 100,
+        height: 72,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 0,
+              color: Colors.grey,
+              offset: Offset(
+                0,
+                1,
+              ),
+            )
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text("$index."),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 44,
+                  height: 44,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(44),
+                      child: Image.network(
+                        'https://picsum.photos/seed/183/600',
+                        width: 44,
+                        height: 44,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Expanded(
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(12, 0, 0, 0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
+                        child: Center(
+                          child: Text(
+                            'Random Name',
+                            style: TextStyle(
+                              fontFamily: 'Readex Pro',
+                              letterSpacing: 0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          '1000 pts',
+                          style: TextStyle(
+                            fontFamily: 'Readex Pro',
+                            letterSpacing: 0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.black,
+                size: 24,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
